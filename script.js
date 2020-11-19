26fbffb88637866d16836f7172993f69ebde8977
smedigo <smedigo@gmail.com>
2020-11-18 17:25:59 -0800

    htmlDocument

---
+++ script.js
@@ -0,0 +1,36 @@
+// WHEN the user clicks the search button
+
+function handleSearch() {
+
+// THEN I get the value that they entered into the search input
+
+function makeWeatherRequest();
+
+}
+
+// NEXT we need to build the URL for the first API request AJAX call
+// Example: URL build the users input
+
+// NEXT, make the request to the    URL with JQUERY AJAX
+
+$ajax(queryURL).then(function(response) {
+
+// THEN get the lat and lng out of the 'response' object
+// NEXT call 'makeonecallrequrest' and pass in the lat and lng    (create new var for lat and lng)
+
+} );
+
+function makeOneCallRequest( lat, lng ) {
+
+    // NEXT we need to build the URL for the first API request AJAX call
+// Example: URL build the users input
+
+// NEXT, make the request to the    URL with JQUERY AJAX
+
+$ajax(queryURL).then(function(response) {
+
+// finish rendering data to the html
+
+} );
+
+}
