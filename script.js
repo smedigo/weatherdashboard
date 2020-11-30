b560c1a8ac03c7a86bd2c08b984de79e91c0f455
smedigo <smedigo@gmail.com>
2020-11-29 16:20:18 -0800

    functions

--- script.js
+++ script.js
@@ -1,107 +1,86 @@
+$(document).ready(function() {
+
 const date = moment();
 
 const searchCity = JSON.parse(localStorage.getItem('storage'))
 
-$(document).ready(handleWeatherInfo)
-$(document).ready(currentWeather)
-
-
-
-
-
 function currentWeather(city) {
   var APIKey = "52aa85fe9180c06fe869a1a3e7d7de19"
   var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+  console.log(queryURL )
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(response => {
-    appendWeatherInfo(response)  //?
+         WeatherInfo(response)
   })
 }
 
 
-$("#searchBtn").on("click", function (event) {
 
-  var city = $("input").val();
-  localStorage.setItem("storage", JSON.stringify(searchCity));
-  currentWeather(city)
-});
 
 //function for current weather
 
-.then(function(response) {
+ function WeatherInfo(response) {
 
-  console.log(queryURL);
+  
 
   console.log(response);
 
   
-  $(".cityName").html("<h2>" + response.name + " Weather Details</h2>");
-  $(".wind").text("Wind Speed: " + response.wind.speed);
-  $(".humidity").text("Humidity: " + response.main.humidity);
-  $(".UVIndex").text("UVIndex: " + response.main.uvindex);
+  $("#cityName").html("<h2>" + response.name + " Weather Details</h2>");
+  $("#wind").text("Wind Speed: " + response.wind.speed);
+  $("#humidity").text("Humidity: " + response.main.humidity);
+  
   
   var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
   
-  $(".temp").text("Temperature (K) " + response.main.temp);
-  $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
+  $("#temperature").text("Temperature (F) " + tempF.toFixed(2));
+  
   
   console.log("Wind Speed: " + response.wind.speed);
   console.log("Humidity: " + response.main.humidity);
   console.log("Temperature (F): " + tempF);
-  console.log("UVIndex:" + response.main.uvindex);
-});
+  
+  handleWeatherInfo(response)
+};
 
 
 
-
-  //function appendWeatherInfo(response) {
-
-//  for (let i = 0; i <= 7; i++) {
-
-//var weatherAppend = $(`#weathercard${i}`);
-//var tempF = (response.daily[i].temp.day - 273.15) * 1.80 + 32
-
-// $(`#card${i}`).text(moment().add(i, "d").format("l"));
-
-//$(`#card-body${i}`).attr('src', `https://openweathermap.org/img/wn/${response.daily[i].weather[0].icon}@2x.png`);
-
-//$("<p>").text('Temp: ' + tempF.toFixed(2) + "°F").appendTo(weatherAppend).addClass('is-size-4');
-
-//}
-//}
-
-function handleWeatherInfo() {
-
-  var APIKey = "52aa85fe9180c06fe869a1a3e7d7de19"
-  var lat = 47.4444
-  var long = -122.3138
-
-  var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=" + APIKey;
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(response => {
-    appendWeatherInfo(response)
-  })
-}
-// function handleweatherinfo, do I need a loop?
 function handleWeatherInfo(response) {
 
-  for (let i = 0; i <= 7; i++) {
+  const oneCallURL= `https://api.openweathermap.org/data/2.5/onecall?lat=${response.coord.lat}&lon=${response.coord.lon}&exclude=hourly&appid=52aa85fe9180c06fe869a1a3e7d7de19`;
+  
+  $.ajax({
+    url: oneCallURL,
+    method: "GET"
+  }).then(response => {
+    console.log(response)
+    $("#uvIndex").text(response.current.uvi);
+    myFunction(response)
+  })
+}
+// function handleweatherinfo
+function myFunction(response) {
 
-    var weatherAppend = $(`#weathercard${i}`);
+  for (let i = 1; i <= 5; i++) {
+
+    
     var tempF = (response.daily[i].temp.day - 273.15) * 1.80 + 32
-
-
+    $(`#day${i}`).text(moment().add(i,"d").format("l"))
+    $(`#weeklyTemp${i}`).text(tempF.toFixed(2))
+    $(`#weeklyHumidity${i}`).text(response.daily[i].humidity)
   }
+}
 
 
-
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
+  var city = $("input").val();
+  localStorage.setItem("storage", JSON.stringify(searchCity));
+  currentWeather(city)
+});
 
 
 
@@ -142,3 +121,5 @@ function handleWeatherInfo(response) {
 //} );
 
 //}
+
+});
